3f71edb67e6756d28e800bc09caacf78fb3d05ae
Farouk-ayo <mustaphafarouk41@gmail.com>
2025-09-24 11:09:53 +0100

    composition api and options api

---
+++ vue-2/src/App2.vue
@@ -0,0 +1,41 @@
+// Options API
+
+<script>
+export default {
+  data() {
+    return {
+      name: "farouk",
+      status: "active",
+      tasks: ["task one", "task two", "task three"],
+      link: "http://google.com/",
+    };
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === "active") {
+        this.status = "pending";
+      } else if (this.status === "pending") {
+        this.status = "inactive";
+      } else {
+        this.status = "active";
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">user is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>user is inactive</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <!-- <a v-bind:href="link">Click this link</a> -->
+  <a :href="link">Click this link</a><br />
+  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
+  <button @click="toggleStatus">Change Status</button>
+</template>
